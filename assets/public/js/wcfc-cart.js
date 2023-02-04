516350a7feb2bb52d43cbd7016a6dd08910728d7
cupid chakma <greendaycu20@gmail.com>
2023-02-04 10:41:42 +0600

    code redundency removed

--- assets/public/js/wcfc-cart.js
+++ assets/public/js/wcfc-cart.js
@@ -1,11 +1,12 @@
 (function (window) {
   "use strict";
 
-  function classReg(className) {
-    return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
-  }
+  let hasClass, addClass, removeClass;
 
-  var hasClass, addClass, removeClass;
+   let bodyEl = document.body,
+  openbtn    = document.getElementById("open-button"),
+  closebtn   = document.getElementById("wcfc-close-button"),
+  isOpen     = false;
 
   if ("classList" in document.documentElement) {
     hasClass = function (elem, c) {
@@ -17,70 +18,30 @@
     removeClass = function (elem, c) {
       elem.classList.remove(c);
     };
-  } else {
-    hasClass = function (elem, c) {
-      return classReg(c).test(elem.className);
-    };
-    addClass = function (elem, c) {
-      if (!hasClass(elem, c)) {
-        elem.className = elem.className + " " + c;
-      }
-    };
-    removeClass = function (elem, c) {
-      elem.className = elem.className.replace(classReg(c), " ");
-    };
   }
 
-  function toggleClass(elem, c) {
-    var fn = hasClass(elem, c) ? removeClass : addClass;
-    fn(elem, c);
-  }
-
-  var classie = {
-    // full names
+  let classie = {
     hasClass: hasClass,
     addClass: addClass,
     removeClass: removeClass,
     toggleClass: toggleClass,
-    // short names
     has: hasClass,
     add: addClass,
     remove: removeClass,
     toggle: toggleClass,
   };
 
-  // transport
   if (typeof define === "function" && define.amd) {
-    // AMD
     define(classie);
   } else {
-    // browser global
     window.classie = classie;
   }
 
-  var bodyEl = document.body,
-    // content = document.querySelector( '.content-wrap' ),
-    openbtn = document.getElementById("open-button"),
-    closebtn = document.getElementById("wcfc-close-button"),
-    isOpen = false;
-
-  function init() {
-    initEvents();
-  }
+ 
 
   function initEvents() {
     openbtn.addEventListener("click", toggleMenu);
-    if (closebtn) {
-      closebtn.addEventListener("click", toggleMenu);
-    }
-
-    // close the menu element if the target it´s not the menu element or one of its descendants..
-    // content.addEventListener( 'click', function(ev) {
-    // 	var target = ev.target;
-    // 	if( isOpen && target !== openbtn ) {
-    // 		toggleMenu();
-    // 	}
-    // } );
+    closebtn.addEventListener("click", toggleMenu);
   }
 
   function toggleMenu() {
@@ -92,5 +53,5 @@
     isOpen = !isOpen;
   }
 
-  init();
+  initEvents();
 })(window);
