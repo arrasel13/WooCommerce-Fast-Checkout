2518b04099a38d040ca006df61ef80e4a080e1c2
cupid chakma <greendaycu20@gmail.com>
2023-02-04 15:27:35 +0600

    cart css initialized

--- views/cart.php
+++ views/cart.php
@@ -2,205 +2,52 @@
 
 ?>
 
-<div class="wcfc-container">
-    <div class="wcfc-container-wrap">
-        <div class="wcfc-header">
-            <div class="wcfc-header-text">
-                <p><i class="fa-solid fa-cart-shopping"></i> Shopping Cart <span>(3)</span> </p>
-                <button id="wcfc-clear-all"><i class="fa-regular fa-trash-can"></i> Clear All </button>
+<div class="wcfc-parent-wrapper">
+    <ul class="wcfc-header">
+        <li class="wcfc-item-1"><h2>Shopping Cart(5)</h2></li>
+        <li class="wcfc-item-2">
+            <svg width="20" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M21 5.98C17.67 5.65 14.32 5.48 10.98 5.48C9 5.48 7.02 5.58 5.04 5.78L3 5.98" stroke="#292D32" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M8.5 4.97L8.72 3.66C8.88 2.71 9 2 10.69 2H13.31C15 2 15.13 2.75 15.28 3.67L15.5 4.97" stroke="#292D32" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M18.85 9.14L18.2 19.21C18.09 20.78 18 22 15.21 22H8.79C6 22 5.91 20.78 5.8 19.21L5.15 9.14" stroke="#292D32" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M10.33 16.5H13.66" stroke="#292D32" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M9.5 12.5H14.5" stroke="#292D32" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+            <span>Clear all</span>
+        </li>
+        <li class="wcfc-item-3">
+            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M9.17 14.83L14.83 9.17" stroke="#292D32" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M14.83 14.83L9.17 9.17" stroke="#292D32" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M9 22H15C20 22 22 20 22 15V9C22 4 20 2 15 2H9C4 2 2 4 2 9V15C2 20 4 22 9 22Z" stroke="#292D32" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+        </li>
+    </ul>
+    <ul class="wcfc-products">
+        <li class="wcfc-product-list">
+            <div class="wcfc-product-info">
+                <img src="<?php echo WCFC_PUBLIC_ASSETS_URL . 'img/img-2.png' ?>"></img>
+                <div class="wcfc-product-info-2">
+                    <p>Trendy Shoes For Men</p>
+                    <p>1 pair X 5</p>
+                    <div class="wcfc-info-group">
+                        <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M11.92 22C17.42 22 21.92 17.5 21.92 12C21.92 6.5 17.42 2 11.92 2C6.42 2 1.92 6.5 1.92 12C1.92 17.5 6.42 22 11.92 22Z" stroke="#292D32" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M7.92 12H15.92" stroke="#292D32" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                        <input type="number" class="wcfc-class" name="wcfc-qty" value="2"></input>
+                        <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z" stroke="#292D32" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M8 12H16" stroke="#292D32" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M12 16V8" stroke="#292D32" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </div>
+                </div>
+                <p class="wcfc-product-price">$50</p>
             </div>
-            <!-- <button class="wcfc-close-button" id="wcfc-close-button">Close Menu</button> -->
-            <button class="fa-solid fa-xmark wcfc-close-button" id="wcfc-close-button"></button>
-        </div>
-        <div class="wcfc-empty-content">
-            <div class="wcfc-empty-content-wrapper">
-                <div class="wcfc-empty-cart-img">
-                    <img src="./img/cart-min 1.png" alt="Empty Cart">
-                </div>
-                <h2>Your cart is empty !</h2>
-                <p>Please add product to your cart list</p>
-            </div>
-        </div>
+        </li>
+        <li>
 
-        <div class="wcfc-main-container">
-            <div class="wcfc-main-container-wrapper">
-                <div class="wcfc-items">
-                    <div class="wcfc-single-item">
-                        <div class="wcfc-item-image">
-                            <div class="wcfc-img-wrapper">
-                                <img src="img/shirt.png" alt="">
-                                <div class="wcfc-item-remove">
-                                    <button class="fa-regular fa-trash-can"></button>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="wcfc-item-details">
-                            <h2 class="wcfc-item-title">Trendy Shoes for Man</h2>
-                            <p><span class="wcfc-item">1 Pair</span> X <span class="wcfc-quantity">5</span></p>
-                            <span class="wcfc-qty">
-                                <div class="wcfc-quantity">
-                                    <form>
-                                        <div class="wcfc-quantity-row">
-                                            <span class="wcfc-quantity-col wcfc-quantity-col-minus wcfc-quantity-button">
-                                                <i class="fa-solid fa-minus"></i>
-                                            </span>
-                                            <span class="wcfc-quantity-col wcfc-quantity-col-input">
-                                                <input type="number" id="wcfc-quantity-121" class="wcfc-input-text wcfc-num-qty text" step="1" min="1" max="" name="wcfc-quantity" value="5" title="Qty" placeholder="" inputmode="numeric">
-                                            </span>
-                                            <span class="wcfc-quantity-col wcfc-quantity-col-plus wcfc-quantity-button">
-                                                <i class="fa-solid fa-plus"></i>
-                                            </span>
-                                        </div>
-                                    </form>
-                                </div>
-                            </span>
-                        </div>
-                        <div class="wcfc-item-price">
-                            <span>$50</span>
-                        </div>
-                    </div>
-                    <div class="wcfc-single-item">
-                        <div class="wcfc-item-image">
-                            <div class="wcfc-img-wrapper">
-                                <img src="img/shirt.png" alt="">
-                                <div class="wcfc-item-remove">
-                                    <button class="fa-regular fa-trash-can"></button>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="wcfc-item-details">
-                            <h2 class="wcfc-item-title">Rubber Slipper</h2>
-                            <p><span class="wcfc-item">1 bunch</span> X <span class="quantity">1</span></p>
-                            <span class="wcfc-qty">
-                                <div class="wcfc-quantity">
-                                    <form>
-                                        <div class="wcfc-quantity-row">
-                                            <span class="wcfc-quantity-col wcfc-quantity-col-minus wcfc-quantity-button">
-                                                <i class="fa-solid fa-minus"></i>
-                                            </span>
-                                            <span class="wcfc-quantity-col wcfc-quantity-col-input">
-                                                <input type="number" id="wcfc-quantity-121" class="wcfc-input-text wcfc-num-qty text" step="1" min="1" max="" name="wcfc-quantity" value="1" title="Qty" placeholder="" inputmode="numeric">
-                                            </span>
-                                            <span class="wcfc-quantity-col wcfc-quantity-col-plus wcfc-quantity-button">
-                                                <i class="fa-solid fa-plus"></i>
-                                            </span>
-                                        </div>
-                                    </form>
-                                </div>
-                            </span>
-                        </div>
-                        <div class="wcfc-item-price">
-                            <span>$2.00</span>
-                        </div>
-                    </div>
-
-                    <div class="wcfc-single-item">
-                        <div class="wcfc-item-image">
-                            <div class="wcfc-img-wrapper">
-                                <img src="img/shirt.png" alt="">
-                                <div class="wcfc-item-remove">
-                                    <button class="fa-regular fa-trash-can"></button>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="wcfc-item-details">
-                            <h2 class="wcfc-item-title">Retro Band Polarized Sungla...</h2>
-                            <p><span class="wcfc-item">1 packet</span> X <span class="quantity">5</span></p>
-                            <span class="wcfc-qty">
-                                <div class="wcfc-quantity">
-                                    <form>
-                                        <div class="wcfc-quantity-row">
-                                            <span class="wcfc-quantity-col wcfc-quantity-col-minus wcfc-quantity-button">
-                                                <i class="fa-solid fa-minus"></i>
-                                            </span>
-                                            <span class="wcfc-quantity-col wcfc-quantity-col-input">
-                                                <input type="number" id="wcfc-quantity-121" class="wcfc-input-text wcfc-num-qty text" step="1" min="1" max="" name="wcfc-quantity" value="1" title="Qty" placeholder="" inputmode="numeric">
-                                            </span>
-                                            <span class="wcfc-quantity-col wcfc-quantity-col-plus wcfc-quantity-button">
-                                                <i class="fa-solid fa-plus"></i>
-                                            </span>
-                                        </div>
-                                    </form>
-                                </div>
-                            </span>
-                        </div>
-                        <div class="wcfc-item-price">
-                            <span>$1.50</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="wcfc-menu-footer-top">
-                <div class="wcfc-subtotal">
-                    <div class="wcfc-st-left">
-                        <h2>Subtotal:</h2>
-                    </div>
-                    <div class="wcfc-st-right">
-                        <span>$125</span>
-                    </div>
-                </div>
-
-                <div class="wcfc-shipping-method">
-                    <h2>Shipping method:</h2>
-                    <div class="wcfc-shipping-wrapper">
-                        <div class="wcfc-shipping-option">
-                            <label for="wcfc-flat-rate">
-                                <input type="radio" name="wcfc-shipping-option" id="wcfc-flat-rate" class="wcfc-radio__input" checked>
-                                <div class="wcfc-radio__radio"></div>
-                                Flat Rate
-                            </label>
-                            <label for="wcfc-local-pickup">
-                                <input type="radio" name="wcfc-shipping-option" id="wcfc-local-pickup" class="wcfc-radio__input">
-                                <div class="wcfc-radio__radio"></div>
-                                Local Pickup
-                            </label>
-
-                        </div>
-                        <div class="wcfc-shipping-rate">
-                            <span>$125</span>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="wcfc-apply-coupon">
-                    <h2>Apply Coupon:</h2>
-                    <div class="wcfc-coupon-wrapper">
-                        <div class="wcfc-coupon-option">
-
-                            <form action="" class="wcfc-applyCouponCodeForm">
-                                <input type="text" placeholder="Coupon Code" name="wcfc-couponCode" class="wcfc-applyCouponCodeField">
-                                <input type="submit" value="Apply" class="wcfc-applyCouponCodeBtn">
-                            </form>
-
-                        </div>
-                        <div class="wcfc-coupon-value">
-                            <span>-$12</span>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="wcfc-shipping-address">
-                    <h2>Shipping address:</h2>
-                    <p id="wcfc-address">4639 Hart Ridge Road, Saginaw, Michigan, 48603, 989-249-8309, USA.</p>
-                </div>
-            </div>
-
-        </div>
-
-        <div class="wcfc-menu-footer-bottom">
-            <div class="wcfc-bottom-button">
-                <a href="#" class="wcfc-shopping-btn">
-                    <i class="fa-solid fa-cart-shopping"></i> Continue Shopping
-                </a>
-                <a href="#" class="wcfc-view-cart-btn">
-                    <i class="fa-solid fa-cart-shopping"></i> View Cart
-                </a>
-                <a href="#" class="wcfc-checkout-btn">
-                    <i class="fa-regular fa-credit-card"></i> Proceed To Checkout
-                </a>
-            </div>
-        </div>
-    </div>
-    <button class="menu-button" id="open-button">Open Menu</button>
-</div><!-- /container -->
+        </li>
+    </ul>
+</div>
