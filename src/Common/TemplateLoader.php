1f6439361d7614867c830f53f9336342ec5694ec
cupid chakma <greendaycu20@gmail.com>
2023-03-31 23:55:06 +0600

    updated: Template Loader Class

--- src/Common/TemplateLoader.php
+++ src/Common/TemplateLoader.php
@@ -2,11 +2,9 @@
 
 namespace WooFastCart\Common;
 
-class TemplateLoader{
+class TemplateLoader {
 
     static $instance = null;
-
-    static $counter = 0;
 
     static $file_name;
 
@@ -14,7 +12,7 @@ class TemplateLoader{
 
     static $file_paths = [];
 
-    static $filename   = [];
+    static $filename = [];
 
     public function __construct() {
         self::$file_paths['views']        = WCFC_VIEWS_PATH;
@@ -27,33 +25,33 @@ class TemplateLoader{
     }
 
     public static function init() {
-        if( self::$instance == null ) {
+        if ( self::$instance == null ) {
             self::$instance = new self();
         }
         return self::$instance;
     }
 
     public function setAbsolutePath( $path ) {
-        if( isset( self::$file_paths[$path] ) ) {
+        if ( isset( self::$file_paths[$path] ) ) {
             self::$file_path = $path;
             return $this;
         }
     }
 
     public function setFileName( $filename2, $folderName = '' ) {
-        if( ! empty( $filename2 ) && ! empty( $folderName ) ) {
-            self::$file_name = $filename2;
-            self::$filename[self::$file_name] = $folderName.'/'. self::$file_name;
+        if ( ! empty( $filename2 ) && ! empty( $folderName ) ) {
+            self::$file_name                  = $filename2;
+            self::$filename[self::$file_name] = $folderName . '/' . self::$file_name;
         } else {
-            self::$file_name = $filename2;
+            self::$file_name                  = $filename2;
             self::$filename[self::$file_name] = self::$file_name;
         }
         return $this;
     }
 
     public function renderTemplate( $value ) {
-        if( $value === true ) {
-            require_once( self::$file_paths[self::$file_path] . self::$filename[self::$file_name] );
+        if ( $value === true ) {
+            require_once self::$file_paths[self::$file_path] . self::$filename[self::$file_name];
         }
     }
 }
