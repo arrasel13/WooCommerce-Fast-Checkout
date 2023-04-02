a74bb747abaa5110c48d66eaea06c38b35a630d5
cupid chakma <greendaycu20@gmail.com>
2023-04-02 23:11:55 +0600

    updated: css fadeIn/fadeOut Effect

--- assets/public/js/wcfc-cart.js
+++ assets/public/js/wcfc-cart.js
@@ -1,11 +1,17 @@
 (function ($) {
     $(document).ready(function(){
         $('.wfc-cart-wrapper').on('click', function(e){
+            $(this).next().addClass('wfc-show').fadeIn(90, 'linear');
+            $(this).next().next().addClass('wfc-show').fadeIn(90, 'linear');
             $(this).next().removeClass('wfc-hide');
             $(this).next().next().removeClass('wfc-hide');
         });
 
         $('.wcfc-item-3').on('click', function(e){
+            $('.wfc-cart-wrapper').next().removeClass('wfc-show');
+            $('.wfc-cart-wrapper').next().next().removeClass('wfc-show');
+            $('.wfc-cart-wrapper').next().fadeOut(90, 'linear');
+            $('.wfc-cart-wrapper').next().next().fadeOut(90, 'linear');
             $('.wfc-cart-wrapper').next().addClass('wfc-hide');
             $('.wfc-cart-wrapper').next().next().addClass('wfc-hide');
         });
